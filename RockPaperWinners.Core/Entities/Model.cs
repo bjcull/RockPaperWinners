5e003fe57fe0148574a56eb7b6e66b7135cc049c
paul allen <paul.allen@ezidebit.com.au>
2012-12-19 16:41:13 +1000

    Added Core DLL

---
+++ RockPaperWinners.Core/Entities/Model.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RockPaperWinners.Core.Entities
+{
+    public class BaseEntity
+    {
+        public int ID { get; set; }
+    }
+
+    public class UserProfile : BaseEntity
+    {
+        [ForeignKey("BetBand")]
+        public int BetBandID { get; set; }
+
+        public string FullName { get; set; }
+        public string Email { get; set; }
+        public decimal Money { get; set; }
+
+        public BetBand BetBand { get; set; }
+    }
+
+    public class BetBand : BaseEntity
+    {
+        public string BetBandName { get; set; }
+        public decimal MinimumBet { get; set; }
+        public decimal MaximumBet { get; set; }
+
+        public ICollection<UserProfile> Users { get; set; }
+    }
+
+    public class GameResult :  BaseEntity
+    {
+        public DateTime GameDateTime { get; set; }
+        public decimal TotalBet { get; set; }
+        public GameResultOutcome ResultOutcome { get; set; }
+
+        public ICollection<GameResultPlayer> GameResultPlayers { get; set; }
+    }
+
+    public class GameResultPlayer : BaseEntity
+    {
+        [ForeignKey("GameResult")]
+        public int GameResultID { get; set; }
+        
+        [ForeignKey("UserProfile")]
+        public int UserID { get; set; }
+
+        public decimal BetAmount { get; set; }
+        public string Action { get; set; }
+        public GameResultOutcome ResultOutcome { get; set; }
+
+        public UserProfile UserProfile { get; set; }
+        public GameResult GameResult { get; set; }
+    }
+
+    public enum GameResultOutcome
+    {
+        Player1Win = 1,
+        Player2Win,
+        Draw
+    }
+}
---
+++ RockPaperWinners.Core/Entities/RockPaperWinnersContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.Entity;
+
+namespace RockPaperWinners.Core.Entities
+{
+    public class RockPaperWinnersContext : DbContext
+    {
+        public RockPaperWinnersContext() : base() { }
+
+        public DbSet<UserProfile> UserProfiles { get; set; }
+        public DbSet<GameResult> GameResults { get; set; }
+        public DbSet<GameResultPlayer> GameResultPlayers { get; set; }
+        public DbSet<BetBand> BetBands { get; set; }
+    }
+}
