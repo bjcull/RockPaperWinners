47675470e2cdc8650d67b86abc2ab2d31f9c93d7
Ben Cull <ben.cull@ezidebit.com.au>
2012-12-20 11:52:58 +1000

    Added Base Controller

---
+++ RockPaperWinners.Web/Controllers/BaseController.cs
@@ -0,0 +1,40 @@
+﻿using Lunchtizzle.Core.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebMatrix.WebData;
+
+namespace Lunchtizzle.Controllers
+{
+    public class BaseController : Controller
+    {
+        private User _currentUser;
+        protected User CurrentUser
+        {
+            get
+            {
+                if (_currentUser == null)
+                    _currentUser = GetCurrentUser();
+
+                return _currentUser;
+            }
+        }
+
+        private User GetCurrentUser()
+        {
+            if (!WebSecurity.IsAuthenticated)
+                return null;
+
+            using (var db = new LunchtizzleContext())
+            {
+                var user = (from u in db.Users
+                            where u.ID == WebSecurity.CurrentUserId
+                            select u).FirstOrDefault();
+
+                return user;
+            }
+        }
+    }
+}
